74220aa21b1f789e456f50e7f70dfe46eda5d48a
EarvinInk <87079067+EarvinInk@users.noreply.github.com>
2022-07-13 13:57:33 +0530

    lll

--- shop_site/cart/urls.py
+++ shop_site/cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('add/<int:p_id>', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_details, name='cart'),
+    path('detail/', views.cart_details, name='cart'),
     path('remove:<int:p_id>',views.remove_item,name='remove_item'),
     path('delete:<int:p_id>',views.delete_item,name='delete_item'),
 
--- shop_site/shop/models.py
+++ shop_site/shop/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 from django.urls import reverse
 
@@ -17,7 +16,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     def get_url(self):
-        return reverse('shop:productsbycat', args= [self.slug])
+        return reverse('shop:productsbycat', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -28,7 +27,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products', blank=True)
     stock = models.IntegerField()
     availability = models.BooleanField(default=True)
@@ -41,4 +40,4 @@ class Product(models.Model):
         verbose_name_plural = 'Products'
 
     def get_url(self):
-        return reverse('shop:productdetails', args=[self.category.slug,self.slug])
+        return reverse('shop:productdetails', args=[self.category.slug, self.slug])
